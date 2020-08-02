5e17bd3e3e6adc404c894d5b49e4f88702950a52
a-powers <awpowers@gmail.com>
2020-08-02 15:30:20 -0400

    update

---
+++ the-pop-method.py
@@ -0,0 +1,6 @@
+actions_stars = ["Norris", "Seagal", "Van Damme", "Schwarzenegger"]
+
+actions_stars.pop(3)
+# either choose index position or leave blank and it will remove last
+# invoke with a variable name and it will store which ever element you removed
+print(actions_stars)
